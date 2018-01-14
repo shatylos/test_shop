aa8a11731e34fdf158c413ba5c233f4beddbf6c5
Serhii Shatylo <sergey@shatylo.pp.ua>
2018-01-14 19:03:23 +0200

    Added product list page.

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Product;
+
+class ProductController extends Controller
+{
+    private $entityClass = Product::class;
+
+    /**
+     * @Route("/", name="product_list")
+     */
+    public function productList()
+    {
+        $products = $this->getDoctrine()->getRepository($this->entityClass)->findAll();
+
+        return $this->render('shop/list.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+}
